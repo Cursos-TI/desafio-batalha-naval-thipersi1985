92803ae2400ddba4c3f2b9a6733134f8e7e3d4ce
thipersi1985 <202503074216@alunos.estacio.br>
2025-04-27 10:37:25 +0000

    nível aventureiro concluído

--- Desafio_Batalha_Naval.c
+++ Desafio_Batalha_Naval.c
@@ -2,13 +2,15 @@
 
 int main (){
 
-	int mapa [10][10]; //criação da matriz
-	int navio_h [3] = {3, 3, 3}; //criação do navio 1 (posicionado na horizontal)
+	int mapa [10][10];	//criação da matriz
+	int navio_h [3] = {3, 3, 3}; //criação dos navio 1 (posicionado na horizontal)
 	int navio_v [3] = {3, 3, 3}; //criação do navio 2 (posicionado na vertival)
-	int x, y, i1, j1, i2, j2; //variáveis utilizadas para entrada e saída de dados 
-	int teste1, teste2; //variáveis de controle necessárias para teste da estrutura while, na entrada de dados
-	int a, b; // variáveis de controle para a estrutura "do - while", necessárias para implementar os cabeçalhos
-	char *vetorCol [10] = {"   A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}; //cabeçalho das coluna
+	int navio_dd [3] = {3, 3, 3}; //criação do navio 3 (diagonal-direita)
+	int navio_de [3] = {3, 3, 3}; //criação do navio 4 (diagonal-esquerda)
+	int x, y, i1, j1, i2, j2, i3, j3, i4, j4; 	//variáveis utilizadas para entrada e saída de dados 
+	int teste1, teste2, teste3, teste4;	//variáveis de controle necessárias para teste da estrutura while, na entrada de dados
+	int a, b;  // variáveis de controle para a estrutura "do - while", necessárias para implementar os cabeçalhos
+	char *vetorCol [10] = {"   A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};  //cabeçalho das coluna
 	char *vetorLin [10] = {" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10"}; //cabeçalho das linha
 
 	// o código abaixo preenche todas as posições da matriz com 0 (zero).
@@ -21,49 +23,77 @@ int main (){
 		
 	}
 	
-	// entrada de dados: Optei por solicitar do usuário os valores das linhas e colunas dos navios 1 e 2.
+	// entrada de dados
 	printf("*** JOGO DA BATALHA NAVAL ***\n");
-	printf("# Navio na Horizontal #\n");
+	//Navio na Horizontal
+	i1 = 1;
+	j1 = 1;
 	teste1 = 0;
 	while (teste1 == 0) {
-		printf("Digite o número da linha do navio 1 - Entre 1 e 10 \n");
-		scanf("%d", &i1);
-		printf("Digite o número da coluna do navio 1 - Entre 1 e 8 \n");
-		scanf("%d", &j1);
 		if ((i1 < 1 || i1 > 10 || j1 < 1 || j1 > 8) == 1) { 	//esse é o teste feito para saber se excede ou não a matriz, aqui chamada de mapa
 			printf("Excede o mapa! Favor digitar intervalos válidos.\n");
 			teste1 = 0;
-		
+					
 		} else {	//aqui os dados da matriz que foram inicializados por "0", estão sendo substituídos por 3, representando o navio 1
 			mapa [i1 - 1][j1 - 1] = navio_h[0];
 			mapa [i1 - 1][j1 - 1 + 1] = navio_h[1];
 			mapa [i1 - 1][j1 - 1 + 2] = navio_h[2];
 			teste1 = 1;
+			
 		}
 	}
-	
-	printf("# Navio na Vertical #\n");
+		
+	//Navio na Vertical
+	i2 = 2;
+	j2 = 2;
 	teste2 = 0;
 	while (teste2 == 0) {
-		printf("Digite o número da linha do navio 2 - Entre 1 e 8\n");
-		scanf("%d", &i2);
-		printf("Digite o número da coluna do navio 2 - Entre 1 e 10\n");
-		scanf("%d", &j2);
 		if ((i2 < 1 || i2 > 8 || j2 < 1 || j2 > 10) == 1) { 	//esse é o teste feito para saber se excede ou não a matriz, aqui chamada de mapa
 			printf("Excede o mapa! Favor digitar intervalos válidos.\n");
 			teste2 = 0;
-
-		}else if ((i2 == i1) == 1 || (j2 == j1) == 1 || (i1 == j2)) {	//esse é o teste realizado para saber se a posição já se encontra ocupada
-			printf("A posição encontra-se ocupada! Escolha outra posição.\n");	
-			teste2 = 0;
-
+	
 		} else {	//aqui os dados da matriz que foram inicializados por "0", estão sendo substituídos por 3, representando o navio 2
 			mapa [i2 - 1][j2 - 1] = navio_v[0];
 			mapa [i2 - 1 + 1][j2 - 1] = navio_v[1];
 			mapa [i2 - 1 + 2][j2 - 1] = navio_v[2];
 			teste2 = 1;
+			
 		}
 	}		
+	
+	//Navio na Diagonal-Direita
+	i3 = 3;
+	j3 = 3;
+	teste3 = 0;
+	while (teste3 == 0) {
+		if ((i3 < 1 || i3 > 8 || j3 < 1 || j3 > 8) == 1) { 	//esse é o teste feito para saber se excede ou não a matriz, aqui chamada de mapa
+			printf("Excede o mapa! Favor digitar intervalos válidos.\n");
+			teste3 = 0;
+
+		} else {	//aqui os dados da matriz que foram inicializados por "0", estão sendo substituídos por 3, representando o navio 3
+			mapa [i3 - 1][j3 - 1] = navio_dd[0];
+			mapa [i3 - 1 + 1][j3 - 1 + 1] = navio_dd[1];
+			mapa [i3 - 1 + 2][j3 - 1 + 2] = navio_dd[2];
+			teste3 = 1;
+		}
+	}
+
+	//Navio na Diagonal-Esquerda
+	i4 = 5;
+	j4 = 3;
+	teste4 = 0;
+	while (teste4 == 0) {
+		if ((i4 < 1 || i4 > 8 || j4 < 3 || j4 > 10) == 1) { 	//esse é o teste feito para saber se excede ou não a matriz, aqui chamada de mapa
+			printf("Excede o mapa! Favor digitar intervalos válidos.\n");
+			teste4 = 0;
+
+		} else {	//aqui os dados da matriz que foram inicializados por "0", estão sendo substituídos por 3, representando o navio 4
+			mapa [i4 - 1][j4 - 1] = navio_de[0];
+			mapa [i4 - 1 + 1][j4 - 1 - 1] = navio_de[1];
+			mapa [i4 - 1 + 2][j4 - 1 - 2] = navio_de[2];
+			teste4 = 1;
+		}
+	}
 	
 	// criação do cabeçalho de colunas
 	a = 0;
@@ -95,6 +125,7 @@ int main (){
 
 	} while (b < 10);
 
+	
 	return 0;
 
 }
